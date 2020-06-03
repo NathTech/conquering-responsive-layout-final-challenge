1cba8902454cf99e2a3d3fbd4bd9f22957bb8da4
Nathaniel Fenton <nathinabob@gmail.com>
2020-06-03 12:31:13 +0100

    #1 header bar done

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -34,6 +34,17 @@ export const html = () => {
     .pipe(gulp.dest(htmlPaths.build))
 }
 
+const jsPaths = {
+    src: dir.src + 'assets/js/*.js',
+    build: dir.build + 'assets/js',
+}
+
+export const js = () => {
+    return gulp.src(jsPaths.src)
+    .pipe(newer(jsPaths.build))
+    .pipe(gulp.dest(jsPaths.build))
+}
+
 const scssPaths = {
     src: dir.src + 'assets/scss/**/*.scss',
     build: dir.build + 'assets/css',
@@ -58,7 +69,7 @@ export const image = () => {
     .pipe(gulp.dest(imagePaths.build))
 }
 
-export const build = gulp.parallel(html, sass, image)
+export const build = gulp.parallel(html, sass, image, js)
 
 const syncOptions = {
     server: {
@@ -85,13 +96,15 @@ const syncReload = (done) => {
     done()
 }
 
-export const watch = gulp.series(sync, (done) => {
+export const watch = gulp.series(build, sync, (done) => {
 
     gulp.watch(htmlPaths.src, gulp.series(html, syncReload))
 
     gulp.watch(imagePaths.src, image)
 
     gulp.watch(scssPaths.src, sass)
+
+    gulp.watch(jsPaths.src, js)
 
     done()
 })
